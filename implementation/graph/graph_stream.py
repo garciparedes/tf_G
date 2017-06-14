24f9bd04fabe925cb0a988c261ba4f7f1225a745
Sergio Garcia Prado <sergio@garciparedes.me>
2017-06-14 19:59:50 +0200

    Workin on GraphStream naive implementation

--- implementation/graph/graph_stream.py
+++ implementation/graph/graph_stream.py
@@ -1,6 +1,8 @@
 from datasets import DataSets
 from graph.graph import Graph
 
+import tensorflow as tf
+
 
 class GraphStream(Graph):
     def __init__(self, sess, name):
@@ -9,11 +11,20 @@ class GraphStream(Graph):
     def append(self, src, dst):
         if src and dst is None:
             raise ValueError("src and dst must not be None ")
+
         self.edges_df = self.edges_df.append({'src': src, 'dst': dst},
                                              ignore_index=True)
+        self.sess.run(tf.assign(self.A_tf,
+                                tf.scatter_nd(self.E_list, self.m * [1.0],
+                                              [self.n, self.n]),
+                                validate_shape=False))
 
     def remove(self, src=None, dst=None):
         if src and dst is None:
             raise ValueError("src and dst must not be None ")
         self.edges_df = self.edges_df[(self.edges_df['src'] != src) &
                                       (self.edges_df['dst'] != dst)]
+        self.sess.run(tf.assign(self.A_tf,
+                                tf.scatter_nd(self.E_list, self.m * [1.0],
+                                              [self.n, self.n]),
+                                validate_shape=False))
