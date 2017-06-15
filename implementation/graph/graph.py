df6bbc3b9ad10b5d811a843d50bf8052772533f2
Sergio Garcia Prado <sergio@garciparedes.me>
2017-06-15 12:23:37 +0200

    Started to work on tensorboard

--- implementation/graph/graph.py
+++ implementation/graph/graph.py
@@ -24,18 +24,45 @@ class Graph(TensorFlowObject):
         self.n_tf = tf.Variable(float(self.n), tf.float32,
                                 name=self.name + "_n")
         self.run(tf.variables_initializer([self.A_tf, self.n_tf]))
+        self.generate_summaries()
+
+    def generate_summaries(self):
+        self.writer.add_summary(
+            self.run(tf.summary.scalar(self.name + '_vertices', self.n)))
+        self.writer.add_summary(
+            self.run(tf.summary.scalar(self.name + '_edges', self.m)))
+        self.writer.add_summary(
+            self.run(
+                tf.summary.histogram(self.name + '_in-degrees',
+                                     tf.concat(([self.get_in_degrees()],
+                                                [tf.range(0, self.n,
+                                                          dtype=tf.float32)]),
+                                               0))))
+        self.writer.add_summary(
+            self.run(
+                tf.summary.histogram(self.name + '_out-degrees',
+                                     tf.concat(([self.get_out_degrees()],
+                                                [tf.range(0, self.n,
+                                                          dtype=tf.float32)]),
+                                               0))))
 
     @property
     def is_not_sink_vertice(self):
-        return tf.not_equal(tf.reduce_sum(self.A_tf, 1), 0)
+        return tf.not_equal(self.get_out_degrees(), 0)
 
     @property
     def in_degrees(self):
-        return tf.reduce_sum(self.A_tf, 0, keep_dims=True)
+        return self.get_in_degrees(keep_dims=True)
 
     @property
     def out_degrees(self):
-        return tf.reduce_sum(self.A_tf, 1, keep_dims=True)
+        return self.get_out_degrees(keep_dims=True)
+
+    def get_in_degrees(self, keep_dims=False):
+        return tf.reduce_sum(self.A_tf, 0, keep_dims=keep_dims)
+
+    def get_out_degrees(self, keep_dims=False):
+        return tf.reduce_sum(self.A_tf, 1, keep_dims=keep_dims)
 
     def __str__(self):
         return str(self.run(self.A_tf))
