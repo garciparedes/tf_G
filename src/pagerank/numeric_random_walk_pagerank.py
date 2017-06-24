e3a40b67e7c3b481042087bc086e8eca04cce61a
Sergio Garcia Prado <sergio@garciparedes.me>
2017-06-24 10:46:19 +0200

    Minor changes improving performance

--- src/pagerank/numeric_random_walk_pagerank.py
+++ src/pagerank/numeric_random_walk_pagerank.py
@@ -13,18 +13,30 @@ class NumericRandomWalkPageRank(NumericIterativePageRank):
         self.t = tf.Variable(0.0)
         self.run(tf.variables_initializer([self.t]))
 
-        self.iter = [self.v_last.assign(self.v),
-                     self.t.assign_add(1),
-                     self.v.assign(
-                         tf.py_func(self.proof, [self.v], tf.float32) * self.t /
-                         tf.add(self.t,
-                                self.G.n_tf))]
+        update_t = self.t.assign_add(1)
 
-    def proof(self, v):
+        with tf.control_dependencies([update_t]):
+            update_v1 = self.v.assign(
+                self.v.read_value() * self.t.read_value() / tf.add(
+                    self.t.read_value(), self.G.n_tf.read_value()))
+
+        with tf.control_dependencies([update_v1]):
+            update_v2 = tf.assign_add(self.v, tf.py_func(self.proof,
+                                                         [self.G.n_tf, self.t],
+                                                         tf.float32))
+
+        self.iter = [self.v_last.assign(self.v),
+                     update_t,
+                     update_v1,
+                     update_v2]
+
+    def proof(self, n, t):
+        v = np.zeros(shape=[1, 4], dtype='float32')
         selected = np.array([np.random.choice([1, 2, 3]),
                              np.random.choice([2, 3]),
                              np.random.choice([0]),
                              np.random.choice([0, 2])])
         for i in selected:
-            v[0,i] += 1 / self.run(self.G.n_tf + self.t)
+            v[0, i] = 1 / (n + t)
+        print(v)
         return v
