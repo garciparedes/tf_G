f5d2ae4ef1c52e64fe31a33d0fdfc6b374810b87
Sergio Garcia Prado <sergio@garciparedes.me>
2017-06-28 16:51:28 +0200

    Minor change

--- src/tfg_big_data_algorithms/utils/__init__.py
+++ src/tfg_big_data_algorithms/utils/__init__.py
@@ -4,7 +4,7 @@ tfg_big_data_algorithms.utils Module
 """
 
 from .datasets import DataSets
-from .tensorflow_object import TensorFlowObject
+from .tensorflow_object import TensorFlowObject, TF_type
 from .update_edge_notifier import UpdateEdgeNotifier
 from .utils import Utils
 from .vector_convergence import ConvergenceCriterion
